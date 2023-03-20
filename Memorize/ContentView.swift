c0e08577e6ede49c40dd15919bc05b5888ed839d
wcasttro <wcastro.santos10@gmail.com>
2023-03-20 11:52:45 -0300

    componentizando card e alterando Estado do card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+     }
+}
+
+struct CardView: View{
+    
+    @State var isFaceUp: Bool = true
+    
+    var body: some View{
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("🎩").font(.largeTitle).foregroundColor(.orange).padding()
+            }else{
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13 Pro")
+        ContentView()
+            .preferredColorScheme(.light)
+            .previewDevice("iPhone 13 Pro")
     }
 }
